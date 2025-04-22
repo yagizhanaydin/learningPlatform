4ed7e0d9cff362db3abed786fa6708c24bdd972f
Egemen Şanal <egemensanal@gmail.com>
2025-04-22 18:03:18 +0300

    added ui for cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Login from './student/Login';
 import AnaSayfa from './AnaSayfa';
 import Studentregister from './Studentregister';
 import TeacherPanel from './teacher/TeacherPanel';
-import CheckoutForm from './proporties/CheckoutForm'; 
+import CheckoutForm from './proporties/CheckoutForm';
 import Help from './Help';
 import StudentPanel from './student/StudentPanel'
 import Admin from './admin/Admin';
@@ -20,12 +20,12 @@ function App() {
         <Route path="/" element={<AnaSayfa />} />
         <Route path="/register" element={<Studentregister />} />
         <Route path="/teacherpanel" element={<TeacherPanel />} />
-        <Route path="/payment" element={<CheckoutForm />} /> 
-      <Route path="/help" element={<Help/>}/>
-      <Route path="/studentpanel" element={<StudentPanel/>}/>
-      <Route path="adminpanel" element={<Admin/>}/>
-      <Route path="/setting" element={<Settings/>}/>
-      <Route path="/sepet" element={<Sepet/>}/>
+        <Route path="/payment" element={<CheckoutForm />} />
+        <Route path="/help" element={<Help />} />
+        <Route path="/studentpanel" element={<StudentPanel />} />
+        <Route path="/adminpanel" element={<Admin />} />
+        <Route path="/setting" element={<Settings />} />
+        <Route path="/sepet" element={<Sepet />} />
       </Routes>
     </Router>
   );
